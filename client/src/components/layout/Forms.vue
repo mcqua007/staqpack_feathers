977eff5fd39b037ef654b29a414d5e14ec21469d
Sean McQuaid <sean@decrypteddesign.com>
2020-03-29 18:21:24 -0700

    type: feat added forms component

---
+++ client/src/components/layout/Forms.vue
@@ -0,0 +1,49 @@
+    <!-- All The Form Componenets -->
+<template>
+ <div>
+      <ProjectForm></ProjectForm>
+      <!-- <TeamForm></TeamForm>
+      <TaskForm v-bind:projectId="projectId" v-on:taskFormInput="emitUp()"></TaskForm> -->
+ </div>
+</template>
+
+<script>
+//import TaskForm from '@/components/forms/TaskForm.vue'
+import ProjectForm from '@/components/forms/ProjectForm.vue'
+// import TeamForm from '@/components/forms/TeamForm.vue'
+
+export default {
+  name: 'Forms',
+  components: {
+    //TaskForm,
+    ProjectForm,
+    //TeamForm
+  },
+  props: ['projectId'],
+  data () {
+    return {
+      msg: 'Test msg'
+    }
+  },
+  methods: {
+    emitUp(){
+      console.log('Forms Task Emit');
+      this.$emit('taskFormInput');
+    }
+  },
+  mounted(){
+    console.log("Loaded Forms")
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
