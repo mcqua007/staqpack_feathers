e148a9133134432142ad1edac621b79be767e829
Sean McQuaid <sean@decrypteddesign.com>
2020-03-29 18:22:54 -0700

    type: feat | added 'fake' about page

---
+++ client/src/views/About.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about container">
+     <h1> About Staqpack </h1>
+     <div class="row">
+         <div class="col-12 text-center">
+             <img src="https://woobro.design/thumbnails/26/development-illustration-5de17f18abc70.png"/>
+         </div>
+     </div>
+     <p>Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug. 
+         Introduced imprudence see say unpleasing devonshire acceptance son. Exeter longer wisdom gay nor design age. 
+         Am weather to entered norland no in showing service.Nor repeated speaking shy appetite.
+          Excited it hastily an pasture it observe. Snug hand how dare here too. </p>
+     <Footer></Footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Footer from '@/components/layout/Footer.vue'
+export default {
+  name: 'About',
+  components: {
+    Footer
+  }
+}
+</script>
+<style scoped>
+ img{
+  max-width: 550px;
+  width:100%;
+  margin: 25px 0;
+}
+
+</style>
