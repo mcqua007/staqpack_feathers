8729cd4720184243542afa74e681d52e23272f7f
Sean McQuaid <sean@decrypteddesign.com>
2020-03-29 18:21:08 -0700

    type: feat | added a footer componenet

---
+++ client/src/components/layout/Footer.vue
@@ -0,0 +1,94 @@
+<template>
+<footer class="footer">
+   <div class="container">
+    <hr>
+        <div class="row">
+      <div class="col-12 text-center">
+          <span class="logo-style">Staqpack</span>
+      </div>
+    </div>
+    <div class="row">
+     <div class="footer-nav col-12">
+      <div class="link-wrap">
+           <router-link to="/">Home</router-link>
+      </div>
+      <div class="link-wrap">
+           <router-link to="/about">About</router-link>
+      </div>
+       <div class="link-wrap">
+           <router-link to="/contact">Contact Us</router-link>
+      </div>
+      <div class="link-wrap">
+           <router-link to="/login">Log In</router-link>
+      </div>
+     </div>
+    </div>
+
+    <div class="row">
+      <div class="col-12 text-center">
+          <span class="copyright-text"> Made with ❤️ in California  <span class="copyright-vr">|</span>  Staqpack Inc. &copy; {{ year }}</span>
+      </div>
+    </div>
+  </div>
+</footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data(){
+     return{
+         year: new Date().getFullYear()
+     }
+  },
+  components:{
+
+  },
+  computed:{
+
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.footer-nav{
+    margin: 12.5px 0;
+    font-size: 16px;
+}
+hr{
+    margin: 0;
+    padding-bottom: 10px;
+}
+.footer-nav .link-wrap{
+    padding: 0 15px;
+    display: inline-block;
+}
+.footer-nav .link-wrap:not(:last-child){
+    border-right: 1px solid #ccc;
+}
+.logo-style{
+    color: #007bff;
+    font-size: 24px;
+}
+.footer{
+  padding: 10px 0;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+  width:100%;
+}
+.copyright-text{
+    font-size: 13px;
+}
+.copyright-vr{
+    font-size: 18px;
+    vertical-align: middle;
+    padding: 0 5px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+</style>
