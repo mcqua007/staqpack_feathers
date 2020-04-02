815a171f84118ed28f5ac78fea41e2b42c73d99e
Sean McQuaid <sean@decrypteddesign.com>
2020-04-01 17:23:11 -0700

    type:feat | added forms to topbar

--- client/src/components/layout/TopBar.vue
+++ client/src/components/layout/TopBar.vue
@@ -1,18 +1,18 @@
 <template lang="html">
   <div class="top-bar">
     <div class="menu-icon" v-if="$store.getters.sideBarState == true" >
-      <i class="fa fa-times menu-icon-style" id="menu-icon-close" @click="toggleSideBar()"></i>
+      <i class="fa fa-times menu-icon-style" @click="toggleSideBar()"></i>
     </div>
      <div class="menu-icon" v-if="$store.getters.sideBarState == false" >
-      <i @click="toggleSideBar()" class="fa fa-bars menu-icon-style" id="menu-icon" ></i>
+      <i @click="toggleSideBar()" class="fa fa-bars menu-icon-style"></i>
     </div>
     <div class="logo-font top-bar-logo">Staqpack</div>
     <div class="btn-group btn-group-style">
     <button class="add-btn-menu c-white"  @click="toggleDropdown()" id="btnGroupDrop1" data-toggle='dropdown' aria-haspopup="true" aria-expanded="false" >
-      <i :class="{'fa':true, 'fa-plus':true, 'active':(dropdownExpanded == true)}" id="add-task-icon"></i></button>
+      <i :class="{'fa':true, 'fa-plus':true, 'active':(dropdownExpanded == true)}"></i></button>
      <div  v-if="dropdownExpanded" :class="{'dropdown-menu':true,'dropdown-menu-right': true, 'active':(dropdownExpanded == true)}" aria-labelledby="btnGroupDrop1" data-dropdown-task-id="">
-      <!-- <button class="dropdown-item"   type="button" data-showfront-btn-id="" @click="toggleTaskForm()" > Task </button> -->
-      <button class="dropdown-item"   type="button" data-showfront-btn-id="" @click="toggleProjectForm()" > <i class="fa fa-plus margin-r-5"></i> New Project</button>
+       <button class="dropdown-item"   type="button" @click="toggleTaskForm()" ><i class="fa fa-plus margin-r-5"></i> New Task </button>
+      <button class="dropdown-item"   type="button"  @click="toggleProjectForm()" > <i class="fa fa-plus margin-r-5"></i> New Project</button>
       <!-- <button class="dropdown-item"   type="button" data-complete-btn-id=""  @click="toggleTeamForm()"> Team</button> -->
     </div>
   </div>
@@ -31,34 +31,30 @@ export default {
     toggleDropdown(){
       this.dropdownExpanded = !this.dropdownExpanded;
     },
-    // toggleTaskForm () {
-    //   // Hide any other forms open by turning there state to false
-
-    //   // Hide Project Form
-    //   this.$store.commit('hideProjectForm')
-    //   // Hide Team Form
-    //   this.$store.commit('hideTeamForm')
-    //   // Toggle Task From state
-    //   this.$store.commit('toggleTaskForm', this.$store.getters.taskFormState)
-    //   // if form showing/true
-    //   if (this.$store.getters.taskFormState == true) {
-    //     // if in view scroll up to top of page
-    //     window.scrollTo(0, 0)
-    //   }
-    // },
+    toggleTaskForm () {
+      // Hide any other forms open by turning there state to false
+      //sets all forms stats to false 
+       this.$store.commit('hideAllForms');
+     // Toggle Task From state
+       this.$store.commit('toggleTaskForm', this.$store.getters.taskFormState)
+    // if form showing/true
+      if(this.$store.getters.taskFormState == true) {
+        // if in view scroll up to top of page -later maybe make a modal
+        this.toggleDropdown(); //close drop down menu
+        window.scrollTo(0, 0)
+      }
+    },
     toggleProjectForm () {
       // Hide any other forms open by turning there state to false
+      //sets all forms stats to false 
+       this.$store.commit('hideAllForms');
 
-      // Hide Task Form
-    //   this.$store.commit('hideTaskForm')
-    //   // Hide Team Form
-    //   this.$store.commit('hideTeamForm')
       // Toggle Project Form state
       this.$store.commit('toggleProjectForm', this.$store.getters.projectFormState)
       // if form showing/true
       if (this.$store.getters.projectFormState == true) {
-        // if in view scroll up to top of page
-        this.toggleDropdown();
+        // if in view scroll up to top of page -later maybe make a modal
+        this.toggleDropdown(); //close drop down menu
         window.scrollTo(0, 0)
       }
     },
