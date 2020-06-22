59f8213396b05223e0c80596f235a3cd08719539
Sean McQuaid <sean@decrypteddesign.com>
2020-06-21 19:35:50 -0700

    type: fea t| added away to delteTask making tasks reactive

--- client/src/components/TaskCard.vue
+++ client/src/components/TaskCard.vue
@@ -188,9 +188,8 @@
           },
         };
 
-        this.$store.dispatch("deleteTasks", data).then((res) => {
-          console.log("Task Deleted res: ", res);
-          this.taskStyle = { display: "none" };
+        this.$store.dispatch("deleteTasks", data).then(() => {
+          this.$store.commit("filterCurrentProjectTasks", projectId); //re-filter the tasks for the current project sincesoem have been deleted
         });
       },
       toggleBack() {
