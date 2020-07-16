3798d9f59224cd8ae06a295d696858131929db4e
Sean McQuaid <sean@decrypteddesign.com>
2020-07-15 20:22:00 -0700

    type: feat | add child routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,6 +4,9 @@ import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Contact from '../views/Contact.vue'
 import Main from '@/components/Main'
+import Project from '@/components/layout/Project'
+import AllTasks from '@/components/layout/AllTasks'
+import Settings from '@/components/settings/Settings'
 import SignUp from '@/components/auth/SignUp'
 import Login from '@/components/auth/Login'
 import store from '../store'
@@ -11,8 +14,7 @@ import store from '../store'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'Home',
     component: Home
@@ -31,16 +33,31 @@ const routes = [
     path: '/app',
     name: 'Main',
     component: Main,
+    children: [{
+        name: 'Project',
+        path: '/app/project/:id',
+        component: Project,
+      },
+      {
+        name: 'Settings',
+        path: '/app/user/settings/',
+        component: Settings,
+      },
+      {
+        name: 'AllTasks',
+        path: '/app/tasks/',
+        component: AllTasks,
+      }
+    ],
     meta: {
       requiresAuth: true
     }
-   },
-   {
-     path: '/signup',
-     name: 'SignUp',
-     component: SignUp
   },
   {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
+  }, {
     path: '/login',
     name: 'Login',
     component: Login
@@ -53,14 +70,13 @@ const router = new VueRouter({
 
 router.beforeEach((to, from, next) => {
   const currentUser = store.state.user
-  console.log("router - current user", currentUser);
+  // console.log("router - current user", currentUser);
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
 
   if (requiresAuth && !currentUser) {
     console.log("in next login");
     next('login')
-  } 
-  else {
+  } else {
     console.log("in next");
     next()
   }
