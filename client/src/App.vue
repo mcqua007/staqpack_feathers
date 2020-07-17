fea6dc8fff650fbbb4a9f5b21a13e61e98cb7dd3
Sean McQuaid <sean@decrypteddesign.com>
2020-07-17 15:11:50 -0700

    type: chore | cleane dup unused code

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,31 +6,18 @@
 </template>
 
 <script>
-//import Main from './components/Main.vue'
-//import Footer from '@/components/layout/Footer.vue'
-
-export default {
-  name: 'App',
-  components: {
-   //Footer,
-  },
-  mounted(){
-    // //init data 
-    //  this.$store.dispatch('fetchProjects').then((res) =>{
-    //    console.log("App Vue: - Called fetchProjects", res);
-    //    this.$store.commit('setLoading', false);
-    //   });
-  }
-}
+  export default {
+    name: "App",
+    components: {},
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
