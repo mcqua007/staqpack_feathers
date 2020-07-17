5b641c6d38eb451dd124a4ef3c2441348e317982
Sean McQuaid <sean@decrypteddesign.com>
2020-07-16 21:04:18 -0700

    type: chore | formatted

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,85 +1,88 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
-import Contact from '../views/Contact.vue'
-import Main from '@/components/Main'
-import Project from '@/components/layout/Project'
-import AllTasks from '@/components/layout/AllTasks'
-import Settings from '@/components/settings/Settings'
-import SignUp from '@/components/auth/SignUp'
-import Login from '@/components/auth/Login'
-import store from '../store'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Contact from "../views/Contact.vue";
+import Main from "@/components/Main";
+import Project from "@/components/layout/Project";
+import AllTasks from "@/components/layout/AllTasks";
+import Settings from "@/components/settings/Settings";
+import SignUp from "@/components/auth/SignUp";
+import Login from "@/components/auth/Login";
+import store from "../store";
 
+Vue.use(VueRouter);
 
-Vue.use(VueRouter)
-
-const routes = [{
-    path: '/',
-    name: 'Home',
-    component: Home
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: "/about",
+    name: "About",
+    component: About,
   },
   {
-    path: '/contact',
-    name: 'Contact',
-    component: Contact
+    path: "/contact",
+    name: "Contact",
+    component: Contact,
   },
   {
-    path: '/app',
-    name: 'Main',
+    path: "/app",
+    name: "Main",
     component: Main,
-    children: [{
-        name: 'Project',
-        path: '/app/project/:id',
+    children: [
+      {
+        name: "Project",
+        path: "/app/project/:id/:name",
         component: Project,
       },
       {
-        name: 'Settings',
-        path: '/app/user/settings/',
+        name: "Settings",
+        path: "/app/user/settings/",
         component: Settings,
       },
       {
-        name: 'AllTasks',
-        path: '/app/tasks/',
+        name: "AllTasks",
+        path: "/app/tasks/",
         component: AllTasks,
-      }
+      },
     ],
     meta: {
-      requiresAuth: true
-    }
+      requiresAuth: true,
+    },
   },
   {
-    path: '/signup',
-    name: 'SignUp',
-    component: SignUp
-  }, {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  }
-]
+    path: "/signup",
+    name: "SignUp",
+    component: SignUp,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  mode: "history", //added to get rid of hashbang in url
+  routes,
+});
 
 router.beforeEach((to, from, next) => {
-  const currentUser = store.state.user
-  // console.log("router - current user", currentUser);
-  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const currentUser = store.state.user;
+  console.log("router - beforeEach | to: ", to, "From: " + from);
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
 
   if (requiresAuth && !currentUser) {
     console.log("in next login");
-    next('login')
+    next("login");
   } else {
     console.log("in next");
-    next()
+    next();
   }
-})
+});
 
 export default router;
