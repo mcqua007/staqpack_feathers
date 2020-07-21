c1a2b3e078496b25900a6de5c600931a13257477
Sean McQuaid <sean@decrypteddesign.com>
2020-07-20 20:46:24 -0700

    type: feat | added commit serch for comparison

--- server/src/hooks/check-github-commits.js
+++ server/src/hooks/check-github-commits.js
@@ -6,7 +6,7 @@ const {
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
   return async context => {
-    console.log('Webhook - Context: ', context.data);
+    console.log('Webhook - Context: ', context.data.commits);
     let query = {
       repoId: context.data.repository.id,
     };
@@ -14,10 +14,34 @@ module.exports = (options = {}) => {
     let res = await context.app.service('github-repositories').find(null, {
       query
     });
+    let projectId = res.projectId;
+    let tasks = await context.app.service('tasks').find(null, {
+      projectId: projectId
+    });
+
     console.log('Response From GitHubRepos', res);
-    //   .then(() => {
-    //   console.log('Response From Projects', res);
-    // });
+    console.log('TaskData Res: ', tasks);
+
+    let commits = context.data.commits;
+    let feats = [];
+    commits.forEach(function (commit, i) {
+      console.log('InforEach', commit, i);
+
+      //FINE UP TO HERE
+      var searchForFeat = commit.search('@feat:');
+      console.log('Search: ', searchForFeat);
+      var sliceStrFeat = '';
+      if (searchForFeat != -1) {
+        console.log('Match! Feat Position:', searchForFeat);
+        sliceStrFeat = commit.slice(this.addToString(searchForFeat, 6));
+      }
+
+      console.log('Sliced Str: ', sliceStrFeat.trim().toLowerCase());
+      feats.push(sliceStrFeat.trim().toLowerCase()); //push onto feats array
+    });
+    // let userId = res.createdBy;
+
+
 
     return context;
   };
