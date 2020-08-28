6ae76a3c2443b0c0307c9896352fad9bd1b8aa10
Sean McQuaid <sean@decrypteddesign.com>
2020-08-27 18:46:56 -0700

    type: feat | remove commented out code

--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -4,12 +4,6 @@
     <SideBar></SideBar>
     <div class="container margin">
       <Forms></Forms>
-      <!-- Commented out because I;'m trying to use router view rather then use if statments - incommented below and remove router-view to go back to how it was-->
-      <!-- <Project v-if="$store.getters.projectState == true"></Project> -->
-      <!-- <Settings v-if="$store.getters.settingsState == true"></Settings>
-      <AllTasks v-if="$store.getters.allTasksState == true"></AllTasks> -->
-
-      <!-- may be better to not have key and use the watcher on project.vue -->
       <router-view :key="$route.fullPath"></router-view>
     </div>
   </div>
@@ -20,11 +14,6 @@
   import TopBar from '@/components/layout/TopBar.vue';
   import SideBar from '@/components/layout/SideBar.vue';
   import Forms from '@/components/layout/Forms.vue';
-  //Commented out because I;'m trying to use router view rather then use if statments
-
-  //import Settings from "@/components/settings/Settings.vue";
-  //import Project from "@/components/layout/Project.vue";
-  //import AllTasks from "@/components/layout/AllTasks.vue";
 
   export default {
     name: 'Main',
@@ -37,11 +26,7 @@
     components: {
       TopBar,
       SideBar,
-      Forms,
-      //Commented out because I;'m trying to use router view rather then use if statments
-      //Project,
-      //Settings,
-      //AllTasks,
+      Forms
     },
     methods: {
       mainClick(event) {
