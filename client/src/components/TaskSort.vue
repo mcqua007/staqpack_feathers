3c29c6360aa77fe0db73aa2d26eb848c47f94288
Sean McQuaid <sean@decrypteddesign.com>
2020-12-21 18:52:17 -0800

    type: chore |  prettier

--- client/src/components/TaskSort.vue
+++ client/src/components/TaskSort.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="push-left">
-    <select class="form-control sort-select" v-model="sortVal" @change="sortTasks">
+    <select
+      class="form-control sort-select"
+      v-model="sortVal"
+      @change="sortTasks"
+    >
       <option value="oldest">Old Tasks</option>
       <option value="newest">Newest Tasks</option>
       <option value="high">Highest Priority</option>
@@ -10,51 +14,51 @@
 </template>
 
 <script>
-  export default {
-    name: 'TaskSort',
-    data() {
-      return {
-        sortVal: 'oldest',
-      };
+export default {
+  name: "TaskSort",
+  data() {
+    return {
+      sortVal: "oldest"
+    };
+  },
+  methods: {
+    sortTasks() {
+      this.$store.commit("SORT_TASKS", this.sortSwitch(this.sortVal)); //commit to store
     },
-    methods: {
-      sortTasks() {
-        this.$store.commit('SORT_TASKS', this.sortSwitch(this.sortVal)); //commit to store
-      },
-      sortSwitch(val) {
-        switch (val) {
-          case 'newest':
-            return {
-              sortBy: 'createdAt',
-              ascending: false,
-            };
-          case 'oldest':
-            return {
-              sortBy: 'createdAt',
-              ascending: true,
-            };
+    sortSwitch(val) {
+      switch (val) {
+        case "newest":
+          return {
+            sortBy: "createdAt",
+            ascending: false
+          };
+        case "oldest":
+          return {
+            sortBy: "createdAt",
+            ascending: true
+          };
 
-          case 'high':
-            return {
-              sortBy: 'severity',
-              ascending: false,
-            };
-          case 'low':
-            return {
-              sortBy: 'severity',
-              ascending: true,
-            };
-        }
-      },
-    },
-  };
+        case "high":
+          return {
+            sortBy: "severity",
+            ascending: false
+          };
+        case "low":
+          return {
+            sortBy: "severity",
+            ascending: true
+          };
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
-  .push-left {
-    margin-left: auto;
-  }
-  .sort-select {
-    max-width: 160px;
-  }
+.push-left {
+  margin-left: auto;
+}
+.sort-select {
+  max-width: 160px;
+}
 </style>
--- client/src/components/TodoList.vue
+++ client/src/components/TodoList.vue
@@ -9,10 +9,17 @@
           @change="toggleCheckBox(todo._id)"
           class="form-check-input"
         />
-        <label class="form-check-label todo-name" :for="todo._id" v-bind:data-id="todo._id">{{
-          todo.name
-        }}</label>
-        <i class="la la-edit edit-todo" role="button" @click="toggleEditTodo(todo.name)"></i>
+        <label
+          class="form-check-label todo-name"
+          :for="todo._id"
+          v-bind:data-id="todo._id"
+          >{{ todo.name }}</label
+        >
+        <i
+          class="la la-edit edit-todo"
+          role="button"
+          @click="toggleEditTodo(todo.name)"
+        ></i>
       </div>
       <div class="edit-todo-row" v-show="editInputState">
         <input
@@ -22,115 +29,128 @@
           v-on:keyup="checkChange(todo.name)"
         />
         <i
-          :class="{ la: true, 'la-check': todoChange === true, 'la-times': !todoChange }"
+          :class="{
+            la: true,
+            'la-check': todoChange === true,
+            'la-times': !todoChange
+          }"
           role="button"
           @click="saveTodoEdit(todo._id, todo.name)"
         ></i>
-        <i class="la la-trash edit-todo-delete" role="button" @click="deleteTodo(todo._id)"></i>
+        <i
+          class="la la-trash edit-todo-delete"
+          role="button"
+          @click="deleteTodo(todo._id)"
+        ></i>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "TodoList",
-    props: ["todo", "taskOpen"],
-    data() {
-      return {
-        checkedState: this.todo.completed,
-        editInputState: false,
-        editInput: null,
-        todoChange: false,
-        todoStyle: null,
-      };
-    },
-    methods: {
-      toggleEditTodo(todo) {
-        this.editInputState = !this.editInputState;
+export default {
+  name: "TodoList",
+  props: ["todo", "taskOpen"],
+  data() {
+    return {
+      checkedState: this.todo.completed,
+      editInputState: false,
+      editInput: null,
+      todoChange: false,
+      todoStyle: null
+    };
+  },
+  methods: {
+    toggleEditTodo(todo) {
+      this.editInputState = !this.editInputState;
 
-        if (todo) {
-          this.editInput = todo;
-        }
-      },
-      checkChange(todo) {
-        if (this.editInput != todo) {
-          this.todoChange = true;
-        } else {
-          this.todoChange = false;
-        }
-      },
-      toggleCheckBox(id) {  
-         this.$store
-            .dispatch("patchTodo", { id: id, update: { completed: this.checkedState } })
-            .then();
-      },
-      saveTodoEdit(id, original_todo) {
-        if (this.editInput != original_todo) {
-          //save todo in DB
-          this.$store
-            .dispatch("patchTodo", { id: id, update: { name: this.editInput } })
-            .then(() => {
-              document.querySelector('.todo-name[data-id="' + id + '"]').innerHTML = this.editInput; //edit current todo
-              this.todoChange = false;
-              this.editInputState = false; //close editable state
-            });
-        } else {
-          //toggle todo edit state
-          this.toggleEditTodo();
-        }
-      },
-      deleteTodo(id) {
-        let data = { todo: { id: id, query: "" } };
-        this.$store
-          .dispatch("deleteTodos", data)
-          .then(() => {
-            this.todoStyle = { display: "none" };
-          })
-          .catch((e) => {
-            console.log("Delete Todo Error: ", e);
-          });
-      },
+      if (todo) {
+        this.editInput = todo;
+      }
     },
-    mounted() {},
-  };
+    checkChange(todo) {
+      if (this.editInput != todo) {
+        this.todoChange = true;
+      } else {
+        this.todoChange = false;
+      }
+    },
+    toggleCheckBox(id) {
+      this.$store
+        .dispatch("patchTodo", {
+          id: id,
+          update: { completed: this.checkedState }
+        })
+        .then();
+    },
+    saveTodoEdit(id, original_todo) {
+      if (this.editInput != original_todo) {
+        //save todo in DB
+        this.$store
+          .dispatch("patchTodo", { id: id, update: { name: this.editInput } })
+          .then(() => {
+            document.querySelector(
+              '.todo-name[data-id="' + id + '"]'
+            ).innerHTML = this.editInput; //edit current todo
+            this.todoChange = false;
+            this.editInputState = false; //close editable state
+          });
+      } else {
+        //toggle todo edit state
+        this.toggleEditTodo();
+      }
+    },
+    deleteTodo(id) {
+      let data = { todo: { id: id, query: "" } };
+      this.$store
+        .dispatch("deleteTodos", data)
+        .then(() => {
+          this.todoStyle = { display: "none" };
+        })
+        .catch(e => {
+          console.log("Delete Todo Error: ", e);
+        });
+    }
+  },
+  mounted() {}
+};
 </script>
 
 <style lang="css" scoped>
-  .form-check {
-    display: inline-flex;
-    width: 100%;
-  }
-  .form-check i {
-    opacity: 0;
-    transition: opacity 0.225s linear;
-  }
-  .form-check:hover i {
-    opacity: 1;
-  }
-  i.la-edit {
-    margin-left: auto;
-    color: #6c757d;
-  }
-  i.la-times {
-    color: #6c757d;
-  }
-  i.la-check {
-    color: #28a745;
-  }
-  i.edit-todo-delete {
-    color: #dc3545;
-    margin-left: 5px;
-  }
-  .edit-todo-row {
-    display: inline-flex;
-    align-items: center;
-    width: 100%;
-  }
-  .edit-todo-row input[type="text"] {
-    border: none;
-    border-bottom: 1px solid #ced4da;
-    width: 100%;
-    margin-bottom: 7.5px;
-  }
+.form-check {
+  display: inline-flex;
+  width: 100%;
+}
+.form-check i {
+  opacity: 0;
+  transition: opacity 0.225s linear;
+}
+.form-check:hover i {
+  opacity: 1;
+}
+i.la-edit {
+  margin-left: auto;
+  color: #6c757d;
+}
+i.la-times {
+  color: #6c757d;
+}
+i.la-check {
+  color: #28a745;
+}
+i.edit-todo-delete {
+  color: #dc3545;
+  margin-left: 5px;
+}
+.edit-todo-row {
+  display: inline-flex;
+  align-items: center;
+  width: 100%;
+}
+.edit-todo-row input[type="text"] {
+  border: none;
+  border-bottom: 1px solid #ced4da;
+  width: 100%;
+  margin-bottom: 7.5px;
+}
 </style>
