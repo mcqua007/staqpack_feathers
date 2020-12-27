e9e53e7f65c90deaac9137c9d2ea684c0f3fd255
Sean McQuaid <sean@decrypteddesign.com>
2020-12-26 18:12:49 -0800

    type: feat | added number of completed tasks

--- client/src/components/layout/AllTasks.vue
+++ client/src/components/layout/AllTasks.vue
@@ -10,6 +10,7 @@
       <TaskSort></TaskSort>
     </div>
     <hr />
+    <div class="text-left">Completed: {{ totalCompletedTasks }}</div>
     <div class="row">
       <template v-for="task in tasks">
         <TaskCard v-bind:task="task" :key="task._id"></TaskCard>
@@ -20,61 +21,66 @@
 </template>
 
 <script>
-  import TaskCard from '@/components/TaskCard.vue';
-  import TaskSort from '@/components/TaskSort.vue';
-  //import feathersClient from "@/feathers-client-config.js";
-  //import { mapState } from 'vuex';
+import TaskCard from "@/components/TaskCard.vue";
+import TaskSort from "@/components/TaskSort.vue";
+//import feathersClient from "@/feathers-client-config.js";
+//import { mapState } from 'vuex';
 
-  export default {
-    name: 'AllTasks',
-    components: {
-      TaskCard,
-      TaskSort,
+export default {
+  name: "AllTasks",
+  components: {
+    TaskCard,
+    TaskSort
+  },
+  //props: ['responseData'],
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {
+    tasks() {
+      return this.$store.getters.allTasks;
     },
-    //props: ['responseData'],
-    data() {
-      return {};
-    },
-    methods: {},
-    computed: {
-      tasks() {
-        return this.$store.getters.allTasks;
-      },
-    },
-    created() {
-      // feathersClient.service("tasks").on("created", (newTask) => {
-      //   this.tasks.push(newTask);
-      // });
-    },
-    mounted() {},
-  };
+    totalCompletedTasks() {
+      return this.$store.getters.allTasks.filter(
+        task => task.completed === true
+      ).length;
+    }
+  },
+  created() {
+    // feathersClient.service("tasks").on("created", (newTask) => {
+    //   this.tasks.push(newTask);
+    // });
+  },
+  mounted() {}
+};
 </script>
 
 <style lang="css" scoped>
-  .project-header-bar {
-    display: inline-flex;
-    align-items: center;
-    width: 100%;
-  }
-  .sort-select {
-    max-width: 160px;
-    margin-left: auto;
-  }
-  .delete-project-btn {
-    margin-left: auto;
-    font-size: 25px;
-    background: transparent;
-    border: none;
-    color: #dc3545;
-  }
+.project-header-bar {
+  display: inline-flex;
+  align-items: center;
+  width: 100%;
+}
+.sort-select {
+  max-width: 160px;
+  margin-left: auto;
+}
+.delete-project-btn {
+  margin-left: auto;
+  font-size: 25px;
+  background: transparent;
+  border: none;
+  color: #dc3545;
+}
 
-  /* USED IN MAIN OTHER COMPONENS - MAIN *NOT DRY */
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.5s;
-  }
-  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-    opacity: 0;
-  }
-  /*================================*/
+/* USED IN MAIN OTHER COMPONENS - MAIN *NOT DRY */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+/*================================*/
 </style>
